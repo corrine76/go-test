319d7a0b6e55ca78d729f02eaaf0ce84a6fff1b0
liuyao <liuyao@dustess.com>
2023-03-07 16:58:53 +0800

    feat: gotest-3

--- test6/main.go
+++ test6/main.go
@@ -1,11 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
 func main() {
+	defer MeasureTime(context.TODO(), "aaa")()
 	var arr []string
 	arr = append(arr, "a")
 	arr = append(arr, "b")
 	arr = append(arr, "c")
 	fmt.Println(arr)
+}
+
+func MeasureTime(ctx context.Context, funcName string, extraData ...interface{}) func() {
+	start := time.Now()
+	return func() {
+		dur := time.Since(start)
+		fmt.Println(dur.String())
+	}
 }
