a567beb263d115edea1b0892525333f5f30e31fa
liuyao <liuyao@dustess.com>
2023-04-25 17:08:10 +0800

    feat: gotest-13

--- test8/main.go
+++ test8/main.go
@@ -6,18 +6,23 @@ import (
 )
 
 func main() {
-	// 创建一个新的爬虫实例
+	// Instantiate default collector
 	c := colly.NewCollector()
 
-	// 在访问每个页面成功时调用此回调函数
-	c.OnResponse(func(r *colly.Response) {
-		fmt.Println("Response received", r.StatusCode)
-		// 在页面内容中查找演唱会门票信息
-		r.ForEach("li.item", func(_ int, e *colly.HTMLElement) {
-			fmt.Printf("%s %s\n", e.ChildText(".left em"), e.ChildText(".title a"))
-		})
+	// On every a element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		// link := e.Attr("href")
+		fmt.Printf("e %+v\n", e)
+
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		// c.Visit(e.Request.AbsoluteURL(link))
 	})
 
-	// 访问大麦网的演唱会门票页面
-	c.Visit("https://search.damai.cn/search.htm?spm=a2oeg.home.top.ditem_1_1b66f537.KG9P0d&ctl=%E6%BC%94%E5%94%B1%E4%BC%9A&order=1&cty=&sctl=&tsg=0")
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.Visit("https://ssr1.scrape.center/")
 }
